ac9852587a17ff6094114633cfe2518c3d8a54f0
杨 杰铭 <yangjm@mctech.vip>
2025-05-16 18:04:24 +0800

    update ui

--- form-test/TencentDNSTool.xaml.cs
+++ form-test/TencentDNSTool.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -81,26 +82,49 @@ namespace form_test
             {
             }
 
-            var crt = new Credential { SecretId = DNSID.Text, SecretKey = DNSKEY.Text };
-            _client = new DnspodClient(crt, null);
-            var list = await _client.DescribeRecordList(new DescribeRecordListRequest { Domain = "y-theta.cn" });
-
-            _originItems.Clear();
-            foreach (var item in list.RecordList)
+            try
             {
-                if (item.Type != "A")
-                    continue;
+                Data.ItemsSource = null;
+                var crt = new Credential { SecretId = DNSID.Text, SecretKey = DNSKEY.Text };
+                _client = new DnspodClient(crt, null);
+                var list = await _client.DescribeRecordList(new DescribeRecordListRequest { Domain = DNSDOMAIN.Text });
 
-                _originItems.Add(item);
+                _originItems.Clear();
+                foreach (var item in list.RecordList)
+                {
+                    //if (item.Type != "A")
+                    //    continue;
+
+                    _originItems.Add(item);
+                }
+
+                Data.ItemsSource = GetOrderedItem(_originItems);
             }
-
-            Data.ItemsSource = _originItems.ToList();
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
             var dc = (sender as Button)?.DataContext as RecordListItem;
-
+            if (_client != null)
+            {
+                try
+                {
+                    var response = _client.ModifyRecordBatchSync(new ModifyRecordBatchRequest
+                    {
+                        Change = nameof(dc.Value).ToLower(),
+                        ChangeTo = dc.Value,
+                        RecordIdList = new ulong?[] { dc.RecordId }
+                    });
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
         }
 
         private bool _changing = false;
@@ -124,9 +148,71 @@ namespace form_test
             Debug.WriteLine("2222");
             if (_originItems != null)
             {
-                var filtered = _originItems.Where(i => i.Name.Contains(box.Text));
-                Data.ItemsSource = filtered.ToList();
+                var filtered = _originItems.Where(i => IsSubSet(i.Name, box.Text));
+                Data.ItemsSource = GetOrderedItem(filtered);
             }
         }
+
+        private IList<RecordListItem> GetOrderedItem(IEnumerable<RecordListItem> items)
+        {
+            var result = items.ToList();
+            result.Sort((a, b) => Shell.StrCmpLogicalW(a.Name, b.Name));
+            var list = result.GroupBy(r => r.Type);
+            return list.SelectMany(s => s.ToList()).ToList();
+        }
+
+        private bool IsSubSet(string source, string pattern)
+        {
+            if (source is null || string.IsNullOrEmpty(pattern))
+                return true;
+
+            if (pattern.Length > source.Length)
+                return false;
+
+            int index = 0;
+            var sourcelist = source.ToArray();
+            var patternlist = pattern.ToArray();
+            for (int i = 0; i < sourcelist.Length; i++)
+            {
+                var c = sourcelist[i];
+                if (index >= pattern.Length)
+                {
+                    return true;
+                }
+                if (c == patternlist[index])
+                {
+                    index++;
+                }
+            }
+
+            if (index == patternlist.Length)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private void Grid_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            var dc = (sender as Button)?.DataContext as RecordListItem;
+
+        }
+    }
+
+    public class Shell
+    {
+
+        public static int StrCmpLogicalW(string x, string y)
+        {
+            // windows 无法比较这种情况
+            if (x == null && y == null)
+                return 0;
+
+            return __StrCmpLogicalW(x, y);
+        }
+
+        [DllImport("shlwapi.dll", EntryPoint = "StrCmpLogicalW", CharSet = CharSet.Unicode, ExactSpelling = true)]
+        public static extern int __StrCmpLogicalW(string x, string y);
     }
 }
